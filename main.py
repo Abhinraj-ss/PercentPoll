13170d6a0b8d479838a785fbcb09ac8d1abc5c10
Abhin Raj S S <abhinrajss18@gmail.com>
2021-07-18 23:59:44 +0530

    modified html

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/" , methods=["GET","POST"])
 def index():
     return render_template("index.html")
 

342c7c170424c9521e6c4e1ae5e2119959ab48af
Abhin Raj S S <abhinrajss18@gmail.com>
2021-07-19 23:50:06 +0530

    added app route for register

--- __init__.py
+++ __init__.py
@@ -1,26 +1,34 @@
 
 from flask import Flask, request, render_template, redirect, url_for
 
+import psycopg2
+
 app = Flask("percentpoll")
 
 @app.route("/" , methods=["GET","POST"])
 def index():
     return render_template("index.html")
 
-
-@app.route("/dashboard")
+@app.route("/dashboard" , methods=["GET","POST"])
 def dashboard():
     return render_template("dashboard.html")
 
 
 @app.route("/login")
 def login():
+    
     return redirect(url_for(".dashboard"))
-
-
+   
+    
 @app.route("/logout")
 def logout():
     return redirect(url_for(".index"))
+
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    
+    return render_template("register.html")
 
 @app.route("/create")
 def create():

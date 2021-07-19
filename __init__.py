0a1092c3880aa80420ee0a6c721141a5f8b297e5
Abhin Raj S S <abhinrajss18@gmail.com>
2021-07-19 15:26:21 +0530

    renmaed main.py to __init__.py added date input in create.html

---
+++ __init__.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask("percentpoll")
+
+@app.route("/" , methods=["GET","POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/login")
+def login():
+    return redirect(url_for(".dashboard"))
+
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for(".index"))
+
+@app.route("/create")
+def create():
+	
+	return render_template("create.html")
+
+@app.route("/vote" , methods=["GET","POST"])
+def vote():
+	title=request.form['title']
+	pollOption=request.form.getlist('pollOption[]')
+	closing=request.form['closing']
+	print(f"{title} {pollOption} {type(closing)}")
+	return render_template("vote.html",
+							title=title,
+							pollOption=pollOption)
+
+@app.route("/view")
+def veiw():
+	return render_template("view.html")
+	
+

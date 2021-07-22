60ca21f8191fcffb635e392a5358459919a23c88
Abhin Raj S S <abhinrajss18@gmail.com>
2021-07-22 22:29:19 +0530

    added link copy feature in create page

--- static/main.js
+++ static/main.js
@@ -17,4 +17,17 @@ $(document).ready(function() {
 	})
 });
 
+function copyLink() {
+  /* Get the text field */
+  var copyText = document.getElementById("link");
 
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+} 

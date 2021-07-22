ce8fd8e318f2bfcc71435202691c661ace9a3424
Abhin Raj S S <abhinrajss18@gmail.com>
2021-07-22 20:02:31 +0530

    added progress bar for evry options entered

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
 	var add_button      = $(".add_field_button"); //Add button ID
 	
-	var x = 1; //initlal text box count
+	var x = 2; //initlal text box count
 	$(add_button).click(function(e){ //on add input button click
 		e.preventDefault();
 		if(x < max_fields){ //max input box allowed
